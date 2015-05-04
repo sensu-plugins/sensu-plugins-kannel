3f3be2cee50271f2d043b51c2dc7300dfdc71bcd
Matt Jones <mattjones@yieldbot.com>
2015-05-04 04:51:10 -0400

    update repo

--- lib/sensu-plugins-kannel.rb
+++ lib/sensu-plugins-kannel.rb
@@ -1,7 +1,14 @@
+require 'sensu-plugins-kannel/version'
+
+# Load the defaults
+
 #
-# Set gem version
+# Default class
 #
 module SensuPluginsKannel
-  # Gem version
-  VERSION = '0.0.1.alpha.1'
+  class << self
+  end
+
+  class << self
+  end
 end
